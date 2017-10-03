d8bb6e3d035a5642d32df4005c0a5dc6d58d814f
Casey Takahashi <caseytaka@berkeley.edu>
2017-10-02 19:10:41 -0700

    Generate Course model.

---
+++ app/views/courses/_course.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! course, :id, :created_at, :updated_at
+json.url course_url(course, format: :json)
---
+++ db/migrate/20171003015453_create_courses.rb
@@ -0,0 +1,10 @@
+class CreateCourses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.boolean :is_active
+
+      t.timestamps
+    end
+  end
+end
