da94df753a0bc43e384ded7d626095472611bc3a
Casey Takahashi <caseytaka@berkeley.edu>
2017-11-06 13:01:17 -0800

    Update teacher method in course controller

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -12,27 +12,13 @@ class Api::CoursesController < Api::BaseController
 
   def create
     course = Course.new(course_params)
-
-    if course_params.key?(:teacher_id1)
-      if Teacher.exists?(dream_id: course_params[:teacher_id1])
-        t1 = Teacher.where(dream_id: course_params[:teacher_id1])
-        course.teachers << t1
-      else
-        puts("could not find teacher with id " + course_params[:teacher_id1].to_s)
-        return render_error_response(:forbidden, ["Could not find teacher with ID " + course_params[:teacher_id1].to_s])
-      end
+    errors = []
+    update_teacher("teacher_id1", course, errors)
+    update_teacher("teacher_id2", course, errors)
+    if errors.present?
+      puts(errors) # DELETE
+      return render_error_response(:forbidden, errors)
     end
-
-    if course_params.key?(:teacher_id2)
-      if Teacher.exists?(dream_id: course_params[:teacher_id2])
-      t2 = Teacher.where(dream_id: course_params[:teacher_id2])
-      course.teachers << t2
-      else
-        puts("could not find teacher with id " + course_params[:teacher_id2].to_s)
-        return render_error_response(:forbidden, ["Could not find teacher with ID " + course_params[:teacher_id2].to_s])
-      end
-    end
-
     if course.save
       render json: course
     else
@@ -42,6 +28,13 @@ class Api::CoursesController < Api::BaseController
 
   def update
     course = Course.find(params[:id])
+    errors = []
+    update_teacher("teacher_id1", course, errors)
+    update_teacher("teacher_id2", course, errors)
+    if errors.present?
+      puts(errors) # DELETE
+      return render_error_response(:forbidden, errors)
+    end
     if course.update(course_params)
       render json: course
     else
@@ -59,6 +52,37 @@ class Api::CoursesController < Api::BaseController
   end
 
   private
+
+  def update_teacher(t_id, course, errors)
+    if course_params.key?(t_id)
+      course_teacher_id = course.attributes[t_id]
+      params_teacher_id = course_params[t_id]
+
+      # Remove old teacher
+      puts("do not equal each other? ", course_teacher_id != params_teacher_id)
+      if course_teacher_id.present? && course_teacher_id != params_teacher_id
+        puts("REMOVING TEACHER NOWW!!!")
+        puts("course_teacher_id: ", course_teacher_id)
+        puts("params_teacher_id: ", params_teacher_id)
+        puts("TEACHERS BEFORE REMOVE")
+        puts(course.teachers)
+        filtered_teachers = course.teachers.select { |old_t| old_t.dream_id != course_teacher_id }
+        course.teachers = filtered_teachers
+        puts("TEACHERS AFTER REMOVE")
+        puts(course.teachers)
+      end
+
+      # Add teacher
+      if Teacher.exists?(dream_id: params_teacher_id)
+        new_t = Teacher.where(dream_id: params_teacher_id)
+        course.teachers << new_t
+        puts("TEACHERS AFTER ADD")
+        puts(course.teachers)
+      else
+        errors << "No teacher with ID " + params_teacher_id.to_s
+      end
+    end
+  end
 
   def course_params
     params.require(:course).permit(
