f5a51914749a5c5e863a9d2fa63ed193f256781d
Casey Takahashi <caseytaka@berkeley.edu>
2017-11-13 19:23:05 -0800

    Sessions controller CRUD.

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,46 @@
+class Api::SessionsController < Api::BaseController
+  # TODO(caseytaka): fix authentication
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render json: Session.order(:number).all
+  end
+
+  def create
+    session = Session.new(session_params)
+    if session.save
+      render json: session
+    else
+      render_error_response(:forbidden, session.errors.full_messages)
+    end
+  end
+
+  def update
+    session = Session.find(params[:id])
+    if session.update(session_params)
+      render json: session
+    else
+      error_response(session)
+    end
+  end
+
+  def destroy
+    session = Session.find(params[:id])
+    if session.destroy
+      render json: session
+    else
+      error_response(session)
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(
+      :weekday,
+      :start_time,
+      :end_time,
+      :number,
+    )
+  end
+end
