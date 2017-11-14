d4da73bf9324e914af42013a5dd18877ee6196e1
Casey Takahashi <caseytaka@berkeley.edu>
2017-11-13 18:47:32 -0800

    Association between Course and Session.

--- app/models/course.rb
+++ app/models/course.rb
@@ -13,4 +13,5 @@ class Course < ApplicationRecord
   has_many :students
   has_and_belongs_to_many :teachers
   has_many :attendances
+  has_many :sessions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114021342) do
+ActiveRecord::Schema.define(version: 20171114024517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20171114021342) do
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "course_id"
+    t.index ["course_id"], name: "index_sessions_on_course_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -91,4 +93,5 @@ ActiveRecord::Schema.define(version: 20171114021342) do
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sessions", "courses"
 end
