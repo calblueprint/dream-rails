3a950b8633fd6f562338a8c10d997581828da6ed
Casey Takahashi <caseytaka@berkeley.edu>
2017-11-13 18:33:37 -0800

    Create Sessions model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106210141) do
+ActiveRecord::Schema.define(version: 20171114021342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 20171106210141) do
   create_table "courses_teachers", id: false, force: :cascade do |t|
     t.bigint "course_id", null: false
     t.bigint "teacher_id", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "weekday"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
