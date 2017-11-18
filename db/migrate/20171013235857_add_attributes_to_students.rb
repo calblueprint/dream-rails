2df76c0de5accd2c9427154df804665f6782a839
Melody Wei <melodywei861016@gmail.com>
2017-11-17 17:32:11 -0800

    dream_id

--- db/migrate/20171013235857_add_attributes_to_students.rb
+++ db/migrate/20171013235857_add_attributes_to_students.rb
@@ -4,7 +4,6 @@ class AddAttributesToStudents < ActiveRecord::Migration[5.1]
     add_column :students, :primary_contact_relationship, :string
     add_column :students, :primary_contact_phone, :string
     add_column :students, :primary_contact_phone2, :string
-    add_column :students, :dream_id, :string
     add_column :students, :course_id, :integer
     add_column :students, :teacher_id, :integer
   end
