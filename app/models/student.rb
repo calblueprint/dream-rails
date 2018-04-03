fb6f96b4b174b386b97d1b1687a704b02c232b4c
Vanessa Ng <vanesng96@gmail.com>
2018-04-02 22:50:08 -0700

    removed dream id from student validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -25,7 +25,7 @@ class Student < ApplicationRecord
 	enum primary_language: {'Spanish': 0, 'Creole': 1, 'Other': 2}
   has_many :courses_students
   has_many :courses, :through => :courses_students
-  validates :active_participant, :first_name, :last_name, :dream_id, :birthday, :sex, 
-  :address, :document_type, :phone, :phone_2, :facebook_name, :email, :primary_contact, 
-  :primary_contact_phone, presence: true
+  validates :is_active, :first_name, :last_name, :birthday, :sex, 
+  :address, :document_type, :phone, :facebook_name, :email, 
+  :primary_contact, :primary_contact_phone, presence: true
 end
