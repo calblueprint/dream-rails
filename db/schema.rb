72c33fbd38abd7b7e4dfb03fd0f5cb656bcf2f4f
Wilson Wang <wilsonw926@gmail.com>
2018-06-27 15:52:19 -0700

    convert user's teacher ref to string so teacher ref can be email__c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180627202342) do
+ActiveRecord::Schema.define(version: 20180627224242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,7 +142,7 @@ ActiveRecord::Schema.define(version: 20180627202342) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "teacher_id"
+    t.string "teacher_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["teacher_id"], name: "index_users_on_teacher_id"
--- test/models/courses_teacher_test.rb
+++ test/models/courses_teacher_test.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: courses_teachers
+#
+#  course_id  :integer          not null
+#  teacher_id :integer          not null
+#
+
 require 'test_helper'
 
 class CoursesTeacherTest < ActiveSupport::TestCase
